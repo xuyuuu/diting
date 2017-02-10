97f9e400b26a344ecf0a235dad00949f02380e53
xuyuu <15311330977@163.com>
2017-02-10 11:09:46 +0800

    ci by xuyuu, 加入sysctl消息通道

---
+++ os/diting_sysctl.c
@@ -0,0 +1,279 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/mman.h>
+#include <linux/version.h>
+#include <linux/dcache.h>
+#include <linux/jiffies.h>
+#include <linux/sysctl.h>
+#include <linux/err.h>
+#include <linux/namei.h>
+#include <linux/fs_struct.h>
+#include <linux/mount.h>
+#include <linux/pid.h>
+
+#include <net/net_namespace.h>
+#include <linux/netlink.h>
+
+
+#include "diting_sysctl.h"
+
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 18)
+#define CTL_UNNUMBERED -2
+#endif
+
+static struct ctl_table_header *diting_sysctl_table_header;
+
+static uint32_t diting_sysctl_table_procbehavior_reload;
+static uint32_t diting_sysctl_table_procbehavior_switch;
+static uint32_t diting_sysctl_table_accessbehavior_reload;
+static uint32_t diting_sysctl_table_accessbehavior_switch;
+static uint32_t diting_sysctl_table_killerbehavior_reload;
+static uint32_t diting_sysctl_table_killerbehavior_switch;
+static uint32_t diting_sysctl_table_networkbehavior_reload;
+static uint32_t diting_sysctl_table_networkbehavior_switch;
+
+static ctl_table diting_sysctl_register_subtable_proc[] =
+{
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "reload",
+		.data		= &(diting_sysctl_table_procbehavior_reload),
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= &proc_dointvec,
+	},
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "switch",
+		.data		= &(diting_sysctl_table_procbehavior_switch), 
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= &proc_dointvec,
+	},
+	{0}
+};
+
+static ctl_table diting_sysctl_register_subtable_access[] =
+{
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "reload",
+		.data		= &(diting_sysctl_table_accessbehavior_reload),
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= &proc_dointvec,
+	},
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "switch",
+		.data		= &(diting_sysctl_table_accessbehavior_switch), 
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= &proc_dointvec,
+	},
+	{0}
+};
+
+static ctl_table diting_sysctl_register_subtable_killer[] =
+{
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "reload",
+		.data		= &(diting_sysctl_table_killerbehavior_reload),
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= &proc_dointvec,
+	},
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "switch",
+		.data		= &(diting_sysctl_table_killerbehavior_switch), 
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= &proc_dointvec,
+	},
+	{0}
+};
+
+static ctl_table diting_sysctl_register_subtable_network[] =
+{
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "reload",
+		.data		= &(diting_sysctl_table_networkbehavior_reload),
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= &proc_dointvec,
+	},
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "switch",
+		.data		= &(diting_sysctl_table_networkbehavior_switch), 
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= &proc_dointvec,
+	},
+	{0}
+};
+
+
+static ctl_table diting_sysctl_register_subtable[] =
+{
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "proc_behavior",
+		.mode		= 0500,
+		.child		= diting_sysctl_register_subtable_proc,
+	},
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "access_behavior",
+		.mode		= 0500,
+		.child		= diting_sysctl_register_subtable_access,
+	},
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "killer_behavior",
+		.mode		= 0500,
+		.child		= diting_sysctl_register_subtable_killer,
+	},
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "network_behavior",
+		.mode		= 0500,
+		.child		= diting_sysctl_register_subtable_network,
+	},
+	{0}
+};
+
+static ctl_table diting_sysctl_register_table[] =
+{
+	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
+		.ctl_name	= CTL_UNNUMBERED,
+#endif
+		.procname	= "diting",
+		.mode		= 0500,
+		.child		= diting_sysctl_register_subtable,
+	},
+	{0}
+};
+
+int diting_sysctl_module_init(void)
+{
+	if (!(diting_sysctl_table_header = register_sysctl_table(diting_sysctl_register_table
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 19) 
+					,0
+#endif
+					))){
+		printk("Unable To Register Sysctl Table ...\n");
+		return -1;
+	}
+
+	diting_sysctl_table_procbehavior_reload = 0;
+	diting_sysctl_table_procbehavior_switch = 0;
+	diting_sysctl_table_accessbehavior_reload = 0;
+	diting_sysctl_table_accessbehavior_switch = 0;
+	diting_sysctl_table_killerbehavior_reload = 0;
+	diting_sysctl_table_killerbehavior_switch = 0;
+	diting_sysctl_table_networkbehavior_reload = 0;
+	diting_sysctl_table_networkbehavior_switch = 0;
+
+
+	return 0;
+}
+
+static int
+diting_sysctl_module_destroy(void)
+{
+	unregister_sysctl_table(diting_sysctl_table_header);
+
+	return 0;
+}
+
+
+static int
+diting_sysctl_module_chkstatus(sysctl_type_t type)
+{
+	int ret = -1;
+
+	switch(type){
+	case DITING_PROCBEHAVIOR_RELOAD:
+		if(diting_sysctl_table_procbehavior_reload){
+			diting_sysctl_table_procbehavior_reload = 0;	
+			ret = 0;
+		}
+		break;
+	case DITING_PROCBEHAVIOR_SWITCH:
+		if(diting_sysctl_table_procbehavior_switch)
+			ret = 0;
+		break;
+	case DITING_ACCESSBEHAVIOR_RELOAD:
+		if(diting_sysctl_table_accessbehavior_reload){
+			diting_sysctl_table_accessbehavior_reload = 0;	
+			ret = 0;
+		}
+		break;
+	case DITING_ACCESSBEHAVIOR_SWITCH:
+		if(diting_sysctl_table_accessbehavior_switch)
+			ret= 0;
+		break;
+	case DITING_KILLERBEHAVIOR_RELOAD:
+		if(diting_sysctl_table_killerbehavior_reload){
+			diting_sysctl_table_killerbehavior_reload = 0;	
+			ret = 0;
+		}
+		break;
+	case DITING_KILLERBEHAVIOR_SWITCH:
+		if(diting_sysctl_table_killerbehavior_switch)
+			ret = 0;
+		break;
+	case DITING_NETWORDBEHAVIOR_RELOAD:
+		if(diting_sysctl_table_networkbehavior_reload){
+			diting_sysctl_table_networkbehavior_reload = 0;	
+			ret = 0;
+		}
+		break;
+	case DITING_NETWORDBEHAVIOR_SWITCH:
+		if(diting_sysctl_table_networkbehavior_switch)
+			ret = 0;
+		break;
+	default:	
+		break;
+	}
+
+	return ret;
+}
+
+
+struct diting_sysctl_module diting_sysctl_module = 
+{
+	.init		= diting_sysctl_module_init,
+	.chkstatus	= diting_sysctl_module_chkstatus,
+	.destroy	= diting_sysctl_module_destroy,
+};
---
+++ os/diting_sysctl.h
@@ -0,0 +1,24 @@
+#ifndef __diting_sysctl_h__
+#define __diting_sysctl_h__
+
+typedef enum DITING_SYSCTL_TYPE{
+	DITING_PROCBEHAVIOR_RELOAD = 0,
+	DITING_PROCBEHAVIOR_SWITCH,
+	DITING_ACCESSBEHAVIOR_RELOAD,
+	DITING_ACCESSBEHAVIOR_SWITCH,
+	DITING_KILLERBEHAVIOR_RELOAD,
+	DITING_KILLERBEHAVIOR_SWITCH,
+	DITING_NETWORDBEHAVIOR_RELOAD,
+	DITING_NETWORDBEHAVIOR_SWITCH
+}sysctl_type_t;
+
+struct diting_sysctl_module 
+{
+	int (* init)(void);
+	int (* chkstatus)(sysctl_type_t);
+	int (* destroy)(void);
+};
+
+extern struct diting_sysctl_module diting_sysctl_module;
+
+#endif
