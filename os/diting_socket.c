0ad2a1b573756b30fa8a72417ed5ba54f91c9ce2
xuyuu <15311330977@163.com>
2017-03-08 11:19:29 +0800

    ci by xuyuu, 修改了谛听内核部分

--- os/diting_socket.c
+++ os/diting_socket.c
@@ -14,6 +14,7 @@
 #include <linux/version.h>
 #include <linux/in.h>
 #include <net/sock.h>
+#include <linux/byteorder/generic.h>
 #include <net/inet_sock.h>
 
 #include "diting_util.h"
@@ -188,3 +189,135 @@ int diting_module_inside_socket_connect(struct socket *sock, struct sockaddr *ad
 out:
 	return 0;
 }
+
+
+int diting_module_inside_socket_sendmsg(struct socket *sock, struct msghdr *msg, int size)
+{
+	struct sock *sk = NULL;
+	struct inet_sock *inet = NULL;
+	int family, type;
+	char username[64] = {0}, *path = NULL, *name = NULL;
+	struct diting_socket_msgnode *item;
+
+	if(!sock || IS_ERR(sock))
+		goto out;
+	sk = sock->sk;
+	if(!sk || IS_ERR(sk))
+		goto out;
+	inet = (struct inet_sock *)sk;
+	if(!inet || IS_ERR(inet))
+		goto out;
+
+	if(!sock->ops || IS_ERR(sock->ops))
+		goto out;
+
+	family = sock->ops->family;
+	type = sock->type;
+
+	if((family > DITING_SOCKET_FAMILY_MAX) || (type > DITING_SOCKET_TYPE_MAX))
+		goto out;
+
+	item = (struct diting_socket_msgnode *)kmalloc(\
+			sizeof(struct diting_socket_msgnode), GFP_KERNEL);
+	memset(item, 0x0, sizeof(struct diting_socket_msgnode));
+
+	if(current->cred && !IS_ERR(current->cred))
+		item->uid  = current->cred->uid;
+	else
+		item->uid = -1;
+	if(diting_common_getuser(current, username))
+		strncpy(username, "SYSTEM", sizeof("SYSTEM") - 1);
+	strncpy(item->username, username, strlen(username));
+	item->pid = current->pid;
+	item->type = DITING_SOCKET;
+	item->actype = DITING_SOCKET_SENDMSG;
+	strncpy(item->sockfamily, diting_socket_family[family], strlen(diting_socket_family[family]));
+	strncpy(item->socktype, diting_socket_type[type], strlen(diting_socket_type[type]));
+	item->localport = inet->num;
+	item->localaddr = inet->saddr;
+	item->remoteport = ntohs(inet->dport);
+	item->remoteaddr = inet->daddr;
+
+	if(inet->num == 0 || inet->num == 0x16 || 
+			inet->num == 0x3500 || inet->dport == 0x3500){
+		kfree(name);
+		goto out;	
+	}
+
+	path = diting_common_get_name(current, &name, NULL, DITING_FULLFILE_TASK_TYPE);
+	if(path && !IS_ERR(path)){
+		strncpy(item->proc, path, strlen(path));
+		kfree(name);
+	}
+
+	diting_nolockqueue_module.enqueue(diting_nolockqueue_module.getque(), item);
+out:
+	return 0;
+}
+
+
+int diting_module_inside_socket_recvmsg(struct socket *sock, struct msghdr *msg, int size, int flags)
+{
+	struct sock *sk = NULL;
+	struct inet_sock *inet = NULL;
+	int family, type;
+	char username[64] = {0}, *path = NULL, *name = NULL;
+	struct diting_socket_msgnode *item;
+
+	if(!sock || IS_ERR(sock))
+		goto out;
+	sk = sock->sk;
+	if(!sk || IS_ERR(sk))
+		goto out;
+	inet = (struct inet_sock *)sk;
+	if(!inet || IS_ERR(inet))
+		goto out;
+
+	if(!sock->ops || IS_ERR(sock->ops))
+		goto out;
+
+	family = sock->ops->family;
+	type = sock->type;
+
+	if((family > DITING_SOCKET_FAMILY_MAX) || (type > DITING_SOCKET_TYPE_MAX))
+		goto out;
+
+	item = (struct diting_socket_msgnode *)kmalloc(\
+			sizeof(struct diting_socket_msgnode), GFP_KERNEL);
+	memset(item, 0x0, sizeof(struct diting_socket_msgnode));
+
+	if(current->cred && !IS_ERR(current->cred))
+		item->uid  = current->cred->uid;
+	else
+		item->uid = -1;
+	if(diting_common_getuser(current, username))
+		strncpy(username, "SYSTEM", sizeof("SYSTEM") - 1);
+	strncpy(item->username, username, strlen(username));
+	item->pid = current->pid;
+	item->type = DITING_SOCKET;
+	item->actype = DITING_SOCKET_RECVMSG;
+	strncpy(item->sockfamily, diting_socket_family[family], strlen(diting_socket_family[family]));
+	strncpy(item->socktype, diting_socket_type[type], strlen(diting_socket_type[type]));
+	item->localport = inet->num;
+	item->localaddr = inet->saddr;
+	item->remoteport = ntohs(inet->dport);
+	item->remoteaddr = inet->daddr;
+
+	if(inet->num == 0 || inet->num == 0x16 || 
+			inet->num == 0x3500 || inet->dport == 0x3500){
+		kfree(name);
+		goto out;
+	}
+
+	path = diting_common_get_name(current, &name, NULL, DITING_FULLFILE_TASK_TYPE);
+	if(path && !IS_ERR(path)){
+		strncpy(item->proc, path, strlen(path));	
+		kfree(name);
+	}
+
+	diting_nolockqueue_module.enqueue(diting_nolockqueue_module.getque(), item);
+out:
+	return 0;
+}
+
+
